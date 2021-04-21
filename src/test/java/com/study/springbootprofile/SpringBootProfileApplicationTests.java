8e4fa1f341f274eecd222966fb848c9da8559dce
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2021-04-21 15:47:45 +0900

    init: Add spring boot profile spring boot profile, gradle profile

---
+++ src/test/java/com/study/springbootprofile/SpringBootProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springbootprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
